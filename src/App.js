e1bdde33c12b3c5b1ddec1c5c9eb3172ffb2e32d
kavyamohan-24 <kavyamohan2005@gmail.com>
2024-06-27 18:30:24 +0530

    search subscriber

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import AddSubsciber from './components/AddSubsciber';
+import SearchSubscribers from './components/SearchSubscribers';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <AddSubsciber/>
-    </div>
+   <BrowserRouter>
+   <Routes>
+    <Route path="/" element={<AddSubsciber/>}/>
+    <Route path="/search" element={<SearchSubscribers/>}/>
+   </Routes>
+   </BrowserRouter>
   );
 }
 
