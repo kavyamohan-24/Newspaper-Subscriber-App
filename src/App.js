80d5ab09e105ea8390d8183a9f780d6c1bb7f6ee
kavyamohan-24 <kavyamohan2005@gmail.com>
2024-06-27 18:06:19 +0530

    newspaper subscription

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AddSubsciber from './components/AddSubsciber';
+
+function App() {
+  return (
+    <div>
+      <AddSubsciber/>
+    </div>
+  );
+}
+
+export default App;
