006a204306a3deffa5d3d7fe22bc4c699925c2d1
kavyamohan-24 <kavyamohan2005@gmail.com>
2024-06-27 18:44:13 +0530

    delete subscriber

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import AddSubsciber from './components/AddSubsciber';
 import SearchSubscribers from './components/SearchSubscribers';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import DeleteSubscriber from './components/DeleteSubscriber';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
    <Routes>
     <Route path="/" element={<AddSubsciber/>}/>
     <Route path="/search" element={<SearchSubscribers/>}/>
+    <Route path="/delete" element={<DeleteSubscriber/>}/>
    </Routes>
    </BrowserRouter>
   );
